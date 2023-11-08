84c442395faff5acf2be0eda658be45083b1d214
jessicaxu0605 <jessicaxu0605@gmail.com>
2023-11-07 23:23:47 -0500

    add dbServer.js find and create end points

--- se-project-backend/src/dbServer.js
+++ se-project-backend/src/dbServer.js
@@ -1,18 +1,95 @@
 import express from 'express';
 import mysql from 'mysql';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(express.json());
+const port = "3600"
 
 const db = mysql.createPool({
     connectionLimit: 100,
-    host: "127.0.0.1",
-    user: "testing",
-    password: "39f8#Hhfk93f3l23@",
-    database: "se_site_users",
+    host: "database-1.cmxjvvdu8k9n.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "teamcircus",
+    database: "se_site",
     port: "3306"
 });
 
-db.getConnection((err, connection)=> {
-    if(err) throw (err);
-    console.log("yeaaaaa" + connection.threadId);
+//-----------------------------------------
+
+
+
+app.put("/find", (req,res) => {
+    const search = "SELECT * FROM users WHERE email = ?";
+    const email = req.body.email;
+    const sql_query = mysql.format(search, [email])
+
+    db.getConnection(async(err, connection)=> {
+        connection.release();
+
+        if(err) throw (err);
+
+        await connection.query(sql_query,
+            async (err, result) => {
+                if (err) throw (err)
+                console.log("Search Results:")
+                console.log(result[0].email);
+                res.send("Search Results: "+ result[0].email);
+                found = result.length;
+            }
+        );
+    });
 });
+
+
+
+
+app.put("/create", async(req,res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const hashedPW = await bcrypt.hash(req.body.password, 10); //stores hashed password
+    const email = req.body.email;
+
+    const find_query = mysql.format("SELECT * FROM users WHERE email = ?", [email]);
+    const insert_query = mysql.format ("INSERT INTO users VALUES (0, ?, ?, ?, ?)", [firstName, lastName, email, hashedPW]);
+
+    db.getConnection(async(err, connection)=> {
+        if(err) throw (err);
+
+        //search for users registered under the email
+        await connection.query(find_query,
+            async (err, result) => {
+                if (err) throw (err)
+
+                //if there are no accounts registered under the email,
+                if (result.length == 0) {
+                    if(err) throw (err);
+                    
+                    //register the account
+                    await connection.query(insert_query,
+                        async (err, result) => {
+                            connection.release();
+                            if (err) throw (err)
+                            res.send("user created");
+                        }
+                    );
+
+                } 
+
+                //otherwise, don't register new account, alert client
+                else {
+                    console.log("Search Results:")
+                    console.log(result[0].firstName);
+                    res.send(email +" already has an account")
+                }
+            }
+        );
+    });
+});
+
+
+
+
+
+
--- se-project-backend/src/server.js
+++ se-project-backend/src/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
 app.use(express.json()); //if server recieves request with JSON body, automatically parse it and make available at req.body
+
+const db = mysql.createPool({
+    connectionLimit: 100,
+    host: "127.0.0.1",
+    user: "nodeaccess",
+    password: "20940f9$3F30f",
+    database: "se_site_users",
+    port: "3306"
+});
+
+db.getConnection((err, connection)=> {
+    if(err) throw (err);
+    console.log("yeaaaaa" + connection.threadId);
+});
+
 
 app.get('/get', (req, res)=>{
     res.send('Hello');
